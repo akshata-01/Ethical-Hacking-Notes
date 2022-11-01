0970d839fde7023af356d80ae23c9bcbd6ebd2d1
akshata-01 <akshata.0015@gmail.com>
2022-11-01 21:20:42 +0530

    Add a way of executing the commands such that the user will not be able to run other commands. Use lists

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import subprocess #Importing subprocess module 
 
@@ -6,8 +6,19 @@ network_interface = input("What's your network interface?")
 print(network_interface)
 mac_address = input("What MAC address would you like to set?")
 
+#SYNTAX: subprocess.call("exit 1", shell=True) #
+
 subprocess.call("ifconfig", shell=True) #Using this module and call function with 1st argument = command & 2nd as shell variable to True.
 subprocess.call(f"sudo ifconfig {network_interface} down", shell=True) #Disabling interface 
 subprocess.call(f"sudo ifconfig {network_interface} hw ether {mac_address}", shell=True) #Modify option of ether  | Make sure it's 12 characters & separated with : 
 subprocess.call(f"sudo ifconfig {network_interface} up", shell=True) #enabling the interface 
 subprocess.call("sudo ifconfig", shell=True) #To check 
+
+#SYNTAX: subprocess.call(["ls", "-l"]) #
+subprocess.call(["sudo", "ifconfig", network_interface, "down"])
+subprocess.call(["sudo","ifconfig", network_interface, "hw", "ether", mac_address])
+subprocess.call(["sudo","ifconfig", network_interface, "up"])
+
+
+
+

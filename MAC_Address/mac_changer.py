92b170aabf9239e4a89da51d31abf9689cbb80fd
akshata-01 <akshata.0015@gmail.com>
2022-11-02 14:25:25 +0530

    Check if users values are valid using if & parser.error

--- MAC_Address/mac_changer.py
+++ MAC_Address/mac_changer.py
@@ -9,7 +9,16 @@ def get_arguments():
     parser.add_option( "-i" , "--interface", dest="network_interface", help="Interface to change it's MAC Adress") #Options a user will give 
     parser.add_option( "-m" , "--mac", dest="mac_address", help= "New MAC Adress") #Options a user will give 
     
-    return parser.parse_args() #Allows the obj to understand what the user has given 
+    (options, arguments) = parser.parse_args() #Allows the obj to understand what the user has given 
+
+    if not options.network_interface:
+        #code to handle error 
+        parser.error("Please put a valid value of an interface or use --help for more info")
+
+    elif not options.mac_address:
+        #code to handle error 
+        parser.error("Please put a valid value of a mac address or use --help for more info")
+    return options #Variable that holds new_interface and mac address 
 
 def change_mac_address(network_interface, mac_address):
     print("[+] Changing MAC address for " + network_interface + " to " + mac_address)
@@ -17,6 +26,6 @@ def change_mac_address(network_interface, mac_address):
     subprocess.call(["sudo","ifconfig", network_interface, "hw", "ether", mac_address])
     subprocess.call(["sudo","ifconfig", network_interface, "up"])
 
-(options, arguments) = get_arguments()
+options = get_arguments()
 change_mac_address(options.network_interface, options.mac_address)
 

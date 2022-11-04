5f9b7c75eaa80a10849739fb85f1dec0c0550442
akshata-01 <akshata.0015@gmail.com>
2022-11-04 10:46:02 +0530

    Step 1: Create ARP request directing broadcast MAC address asking for IP

--- Network Scanner/network_scanner.py
+++ Network Scanner/network_scanner.py
@@ -4,11 +4,18 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip) #A packet that asks a specific IP 
+    arp_request.show() #Shows details of the package 
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") #This will create a ether object and store an instance in variable boradcast 
-    scapy.ls(scapy.Ether)
+    broadcast.show()
+    arp_request_boradcast = broadcast/arp_request #appending our 2 packtes using / 
+    arp_request_boradcast.show()
+    #scapy.ls(scapy.Ether)
+    #scapy.ls(scapy.ARP) #ls function by scapy gives names of the variables you can set for ARP class 
+    #arp_request.pdst = ip
     #broadcast.dst = ""ff:ff:ff:ff:ff:ff""
     #arp_request.pdst = ip
     #print(arp_request.summary()) #This is a method in ARP class 
-    #scapy.ls(scapy.ARP) #ls function by scapy gives names of the variables you can set for ARP class 
-    print(broadcast.summary())
+    #print(broadcast.summary())
+    print(arp_request_boradcast.summary()) 
+
 scan("192.168.0.1/24") #Range 0-254 

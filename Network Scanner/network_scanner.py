424f4ad1b8cab38fd2c59127c69e544983afc7a8
akshata-01 <akshata.0015@gmail.com>
2022-11-07 11:01:44 +0530

    Improve Network Scanner: Use argparse instead of optparse

--- Network Scanner/network_scanner.py
+++ Network Scanner/network_scanner.py
@@ -1,18 +1,32 @@
 #!usr/bin/env python3
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t" , "--target", dest="target", help="Target IP / IP range.")
+    options = parser.parse_args()
+    return options
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip) #A packet that asks a specific IP 
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff") #This will create a ether object and store an instance in variable boradcast 
     arp_request_boradcast = broadcast/arp_request #appending our 2 packtes using / 
     answered_list = scapy.srp(arp_request_boradcast, timeout=1, verbose = False)[0] #sr is function of sending & receiving while srp actually allows custom ether
-    print("IP\t\t\t\tMAC Address\n-------------------------------------------------")
+    
     clients_list = [] 
     for element in answered_list:
         client_dict = {"ip":element[1].psrc, "mac": element[1].hwsrc}
         clients_list.append(client_dict)
-        print(element[1].psrc +"\t\t\t"+ element[1].hwsrc) #IP of the client + MAC Address #Each layer has ethernet and arp layer | MAC Address 
-    print(clients_list)
+        #print(element[1].psrc +"\t\t\t"+ element[1].hwsrc) #IP of the client + MAC Address #Each layer has ethernet and arp layer | MAC Address 
+    return clients_list
 
-scan("192.168.0.1/24") #Range 0-254 
+def print_result(results_list):
+    print("IP\t\t\t\tMAC Address\n-------------------------------------------------")
+    for element_client in results_list:
+        print(element_client["ip"] + "\t\t\t" + element_client["mac"])
+
+options = get_arguments()
+scan_result = scan(options.target) #Range 0-254 
+print_result(scan_result)

1e84f3c680c7fc3e2fd4c68aeebc7022127542cc
akshata-01 <akshata.0015@gmail.com>
2022-11-07 10:29:40 +0530

    Improving Network Scanner using Dictionary

--- Network Scanner/network_scanner.py
+++ Network Scanner/network_scanner.py
@@ -8,9 +8,11 @@ def scan(ip):
     arp_request_boradcast = broadcast/arp_request #appending our 2 packtes using / 
     answered_list = scapy.srp(arp_request_boradcast, timeout=1, verbose = False)[0] #sr is function of sending & receiving while srp actually allows custom ether
     print("IP\t\t\t\tMAC Address\n-------------------------------------------------")
+    clients_list = [] 
     for element in answered_list:
-        
+        client_dict = {"ip":element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
         print(element[1].psrc +"\t\t\t"+ element[1].hwsrc) #IP of the client + MAC Address #Each layer has ethernet and arp layer | MAC Address 
-        print("-------------------------------------------------------------------")
+    print(clients_list)
 
 scan("192.168.0.1/24") #Range 0-254 

